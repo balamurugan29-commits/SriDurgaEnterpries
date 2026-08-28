5079d14e7d86c9684f379fcf4c3a83a2257e233f
balamurugan29-commits <balamurugan@sridurgaenterprises.com>
2026-08-28 17:03:46 +0530

    chore: remove unused legacy scripts and clean workspace structure

--- Open-EXE-Folder.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-explorer.exe /select,"%~dp0dist-desktop\Sri Durga Enterprises-win32-x64\Sri Durga Enterprises.exe"
--- export-cert.ps1
+++
@@ -1,6 +0,0 @@
-$cert = Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert | Where-Object { $_.Subject -like "*Sri Durga Enterprises*" } | Select-Object -First 1
-if (-not $cert) {
-    $cert = New-SelfSignedCertificate -Type CodeSigningCert -Subject "CN=Sri Durga Enterprises, O=Sri Durga Enterprises, C=IN" -CertStoreLocation "Cert:\CurrentUser\My" -NotAfter (Get-Date).AddYears(10)
-}
-Export-Certificate -Cert $cert -FilePath "$PSScriptRoot\SriDurgaEnterprises.cer" -Force
-Write-Host "SriDurgaEnterprises.cer exported successfully!"
